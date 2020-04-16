60f47201fda4de309a3b43c538c5de4508feecb1
arnas sinha <arnas@gmail.com>
2020-04-16 18:14:02 +0530

    adding build folder to it.

---
+++ build/precache-manifest.7c8db05176a7942210722f1adfe618a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fdf08f4c6242727da6c6f47fac46d8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c12d6d7b99f4b2b13c4f",
+    "url": "/static/css/main.a970f0c1.chunk.css"
+  },
+  {
+    "revision": "74d2db24d5489926b9d4",
+    "url": "/static/js/2.bf27e641.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/static/js/2.bf27e641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c12d6d7b99f4b2b13c4f",
+    "url": "/static/js/main.122a0073.chunk.js"
+  },
+  {
+    "revision": "a826cd5d499804027421",
+    "url": "/static/js/runtime-main.0b74f3b9.js"
+  },
+  {
+    "revision": "9d681c36a4cae7be4624b000918fc429",
+    "url": "/static/media/grocery.9d681c36.jpg"
+  },
+  {
+    "revision": "293acceaa062fccdf2ffbdedb2a62aeb",
+    "url": "/static/media/meat.293accea.jpg"
+  },
+  {
+    "revision": "1ac447c0b7092552fa7a11124fab23fe",
+    "url": "/static/media/medicine.1ac447c0.jpg"
+  },
+  {
+    "revision": "9624600a7d3a361085a294df2dbdb6c7",
+    "url": "/static/media/vegetables.9624600a.jpg"
+  }
+]);
