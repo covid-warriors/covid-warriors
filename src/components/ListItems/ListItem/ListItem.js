6d1d5008c3c3e74d3b7535d1f39287a5405b4eef
arnas sinha <arnas@gmail.com>
2020-04-20 17:19:57 +0530

    list items complete design

--- src/components/ListItems/ListItem/ListItem.js
+++ src/components/ListItems/ListItem/ListItem.js
@@ -1,9 +1,41 @@
 import React, { memo } from 'react';
 import './style.css';
+import Avatar from '../../../assets/img/avatar.svg';
+import DeleteIcon from '../../../assets/img/remove.svg';
 
 const ListItem = (props) => {
   console.log('props--', props);
-  return (<h1>List item</h1>);
+
+  const availabilityHTML = props.item.stock > 0
+    ? <h5 style={{ color: 'green' }}>In stock.</h5>
+    : <h5 Style={{ color: 'red' }}>Out of stock.</h5>
+  return (
+    <div className="list-item">
+      <div className="left-side">
+        <img className="left-hand-section" src={Avatar} alt="item owner" />
+        <div className="right-hand-section">
+          <div className="top">
+            <h4 className="margin-0">{props.item.name}</h4>
+          </div>
+          <div className="middle">
+            {`Available ${props.item.stock} ${props.item.unit}.`}
+          </div>
+          <div className="bottom">
+            {availabilityHTML}
+          </div>
+        </div>
+
+      </div>
+      <div className="middle-side">
+
+      </div>
+      <div className="right-side">
+        <p className="notification">{props.item.notify} notofication</p>
+        <img src={DeleteIcon} alt="delete item" />
+
+      </div>
+    </div>
+  );
 }
 
 export default memo(ListItem);
--- src/components/ListItems/ListItems.js
+++ src/components/ListItems/ListItems.js
@@ -14,16 +14,23 @@ const ListItems = (props) => {
   context.inventoryData.forEach((categoryBasedObject, index) => {
     if (categoryBasedObject.category === selectedCategory) {
       categoryBasedObject.items.forEach((item, count) => {
-        list.push(<ListItem key={'1' + index + count} item={item} />);
+        list.push(<ListItem key={'1' + index + count} item={item} {...props} />);
       });
     }
   });
 
   return (
     <div className="list-wrapper">
-      <div className="item-image"></div>
-      <div className="list-items">
-        {list}
+      <div className={`item-image ${selectedCategory}`}></div>
+      <div className="list-header">
+        <div onClick={() => props.history.push('/dashboard')} className="back-to-dashboard">Back</div>
+        <div className="item-category">{selectedCategory}</div>
+      </div>
+      <div className="items-button-container">
+        <div className="list-items">
+          {list}
+        </div>
+        <button onClick={() => props.history.push('/add-items')} class="btn btn-primary custom-add">Add Item</button>
       </div>
     </div>
   );
